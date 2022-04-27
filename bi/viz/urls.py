c388f90467ba8f2713890c9d3a852461bfe792ce
myphan512 <myphan512@gmail.com>
2022-04-27 12:22:56 +0700

    redesign navbar

--- bi/viz/urls.py
+++ bi/viz/urls.py
@@ -4,17 +4,22 @@ from . import views
 import importlib
 import os
 from os import listdir
+from django.views.generic import TemplateView
 
-### Import python files in dashboards folder
-dashboards = [f for f in listdir((os.path.abspath(os.path.dirname(__file__))+'\dashboards').replace('\\', '/')) if f.endswith('.py')]
-for d in dashboards:
-    d = d[:d.index('.py')]
-    module = importlib.import_module('.dashboards.'+d, package=__package__)
 
 #### REGISTER URLs
 urlpatterns = [
     path('dashboard/<slug:slug>/', views.render_dash),
-    path('', views.render_dashboard_library, name='dashboard_library'),
     path('chart/<slug:slug>/', views.render_chart),
-    path('dashboard_library', lambda request: redirect('dashboard_library', permanent=False)),
+    path('dashboard_library/', views.render_dashboard_library, name='dashboard_library'),
+
 ]
+
+
+### Import dash apps in dashboards files - called but not used
+dashboards = [f for f in listdir((os.path.abspath(os.path.dirname(__file__))+'\dashboards').replace('\\', '/')) if f.endswith('.py')]
+for d in dashboards:
+    d = d[:d.index('.py')]
+    module = importlib.import_module('.dashboards.'+d, package=__package__)
+    ### Register dashboard slug with names to redirect lickable link
+    urlpatterns.append(path('dashboard/'+d, TemplateView.as_view(template_name='viz/dashboards/'+d+'.html'), name=d))
