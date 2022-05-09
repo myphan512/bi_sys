b34ac9602ab10955b646348690e5c178f23dc92c
myphan512 <myphan512@gmail.com>
2022-05-09 16:12:12 +0700

    Update settings.py

--- bi/bi/settings.py
+++ bi/bi/settings.py
@@ -176,7 +176,7 @@ PLOTLY_COMPONENTS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/',
 ]
